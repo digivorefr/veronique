0ce2161f112ac86a24202e17baeb16d6bcda0c2f
Digivore <pierre@digivore.fr>
2022-01-25 21:40:40 +0100

    Add favicon

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -5,6 +5,9 @@ import Layout from 'scripts/pages/Layout.vue';
 
 // Requiring assets here will include it in the Webpack's bundle process.
 require('../styles/index.scss');
+require('../images/icons/favicon-32x32.png');
+require('../images/icons/favicon-16x16.png');
+require('../images/icons/apple-icon-180x180.png');
 
 Vue.use(VueRouter);
 
